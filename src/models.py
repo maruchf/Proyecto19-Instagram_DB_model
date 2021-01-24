74009d14cc11aa82f3495c18a7fa1ce817348425
maruchf <chacon.mariae.ucv@gmail.com>
2021-01-24 01:36:49 +0000

    add changes for the project with SQLAlquemy

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,63 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email= Column(String,unique=True,nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    first_name= Column(String, nullable=False)
+    last_name= Column(String, nullable=False)
+    password= Column(String(15), nullable=False)
+    #relación que permite al objeto hijo referirse al objeto padre directamente,usa el ForeingKey preestablecida
+    posts= relationship("Post",backref="author")
+    comments = relationship("Comment",backref="author_comment")
+    posts_likes =relationship("PostLike",backref="author_post_like")
+    comments_likes= relationship("CommentLike",backref="author_comment_like")
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    image_url = Column(String, unique=True, nullable=False)
+    date_published = Column(DateTime, nullable=False)
+    content = Column(String(300),nullable=True)
+    latitude= Column(Float,nullable=True)
+    longitude= Column(Float, nullable=True)
+    likes=relationship("PostLike", backref="post")
+    comments=relationship("Comment", backref="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    content = Column(String(300),nullable=True)
+    date_published = Column(DateTime, nullable=False)
+    comment_likes = relationship("CommentLike", backref="comment")
+
+class CommentLike(Base):
+    __tablename__ = 'commentlike'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class PostLike(Base):
+    __tablename__ = 'postlike'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def to_dict(self):
         return {}
